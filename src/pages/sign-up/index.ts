bfb7e9574aebcd670f378e9b08825494297d0d75
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-01-24 17:02:56 +0300

    + регистрация проверка полей (+ правки виды валидаций)

--- src/pages/sign-up/index.ts
+++ src/pages/sign-up/index.ts
@@ -1,6 +1,12 @@
 import { template } from "./template";
 import "../profile/styles.scss";
 import Component from "../../services/component";
+import validateTypes from "../../utils/validate-types";
+import {
+  FormValidate,
+  setInputsValidate,
+} from "../../components/form-validate";
+import { form_template } from "./template-form";
 
 const signUpPage = () => {
   const values = {
@@ -18,7 +24,140 @@ const signUpPage = () => {
     error: "Ошибка ошибка ошибка",
     login_link: "Войти",
   };
-  const content = new Component("div", values);
+
+  const inputs = {
+    emailBlock: {
+      attr: {
+        type: "email",
+        name: "email",
+        class: "input-text auth__input-text",
+        placeholder: values.email,
+      },
+      validate: {
+        ...validateTypes.email,
+        class: "auth_error",
+      },
+    },
+    loginBlock: {
+      attr: {
+        type: "text",
+        name: "login",
+        class: "input-text auth__input-text",
+        placeholder: values.login,
+      },
+      validate: {
+        ...validateTypes.login,
+        class: "auth_error",
+      },
+    },
+    nameBlock: {
+      attr: {
+        type: "text",
+        name: "first_name",
+        class: "input-text auth__input-text",
+        placeholder: values.name,
+      },
+      validate: {
+        ...validateTypes.name,
+        class: "auth_error",
+      },
+    },
+    surnameBlock: {
+      attr: {
+        type: "text",
+        name: "second_name",
+        class: "input-text auth__input-text",
+        placeholder: values.surname,
+      },
+      validate: {
+        ...validateTypes.name,
+        class: "auth_error",
+      },
+    },
+    phoneBlock: {
+      attr: {
+        type: "tel",
+        name: "phone",
+        class: "input-text auth__input-text",
+        placeholder: values.phone,
+      },
+      validate: {
+        ...validateTypes.phone,
+        class: "auth_error",
+      },
+    },
+    passwordBlock: {
+      attr: {
+        type: "password",
+        name: "password",
+        class: "input-text auth__input-text",
+        placeholder: values.password,
+      },
+      validate: {
+        ...validateTypes.password,
+        class: "auth_error",
+      },
+    },
+    passwordConfirmBlock: {
+      attr: {
+        type: "password",
+        name: "password_confirm",
+        class: "input-text auth__input-text",
+        placeholder: values.password_confirm,
+      },
+      validate: {
+        ...validateTypes.password,
+        class: "auth_error",
+      },
+    },
+  };
+
+  setInputsValidate(inputs);
+
+  const formButton = new Component("button", {
+    ...values,
+    template: "{{ button }}",
+    attr: {
+      type: "submit",
+      class: "input-button auth__button",
+    },
+  });
+
+  const form = new FormValidate("form", {
+    ...values,
+    ...inputs,
+    buttonBlock: formButton,
+    template: form_template,
+    attr: {
+      class: "auth",
+    },
+    events: {
+      submit: (event: Event) => {
+        event.preventDefault();
+        if (form.checkFields()) {
+          const values = new FormData(form.getContent() as HTMLFormElement);
+          const data: Record<string, FormDataEntryValue> = {};
+          for (const pair of values.entries()) {
+            data[pair[0]] = pair[1];
+          }
+          console.log(data);
+          (form.getContent() as HTMLFormElement).reset();
+          form.children.buttonBlock.setProps({
+            button: "Отправлено",
+            attr: {
+              disabled: "true",
+            },
+          });
+        }
+      },
+    },
+  });
+
+  const content = new Component("div", {
+    ...values,
+    template: template,
+    form: form,
+  });
   return { pageTitle: values.pageTitle, content: content };
 };
 
--- src/pages/sign-up/template.ts
+++ src/pages/sign-up/template.ts
@@ -1,36 +1,6 @@
 export const template = `
 [[ temp_nav? ]]
 <main class="auth-wrapper">
-    <form class="auth">
-        <h1 class="auth__title">Регистрация</h1>
-        <p class="auth__text"></p>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='email' &className='input-text auth__input-text' &placeholder='{{ email }}' ]]
-        </label>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='login' &className='input-text auth__input-text' &placeholder='{{ login }}' ]]
-        </label>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='first_name' &className='input-text auth__input-text' &placeholder='{{ name }}' ]]
-        </label>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='second_name' &className='input-text auth__input-text' &placeholder='{{ surname }}' ]]
-        </label>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='phone' &className='input-text auth__input-text' &placeholder='{{ phone }}' ]]
-        </label>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='password' &type='password' &className='input-text auth__input-text' &placeholder='{{ password }}' ]]
-        </label>
-        <label class="auth__input-text-wrapper">
-            [[ input? &name='password_confirm' &type='password' &className='input-text auth__input-text' &placeholder='{{ password_confirm }}' ]]
-        </label>
-        <div class="auth__button-wrapper">
-            <button type="submit" class="input-button auth__button">{{ button }}</button>
-        </div>
-        <div class="auth__link">
-            [[ link? &href='/sign-in' &title='{{ login_link }}' ]]
-        </div>
-    </form>
+    {{form}}
 </main>
 `;
