98b81e5e5b52da477c06ddf9904292ffe3194dfc
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-02-14 13:26:58 +0300

    + route

---
+++ src/services/router/route.ts
@@ -0,0 +1,44 @@
+import render from "../../utils/render";
+
+class Route {
+  _pathname: string;
+  _blockClass: any;
+  _block: any;
+  _props: any;
+
+  constructor(pathname: string, view: any, props: any) {
+    this._pathname = pathname;
+    this._blockClass = view;
+    this._block = null;
+    this._props = props;
+  }
+
+  navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  leave() {
+    if (this._block) {
+      this._block.hide();
+    }
+  }
+
+  match(pathname: string) {
+    return pathname === this._pathname;
+  }
+
+  render() {
+    if (!this._block) {
+      this._block = new this._blockClass();
+      render(this._props.rootQuery, this._block);
+      return;
+    }
+
+    this._block.show();
+  }
+}
+
+export default Route;
---
+++ src/services/router/router.ts
@@ -0,0 +1,63 @@
+class Router {
+  constructor(rootQuery) {
+    if (Router.__instance) {
+      return Router.__instance;
+    }
+
+    this.routes = [];
+    this.history = window.history;
+    this._currentRoute = null;
+    this._rootQuery = rootQuery;
+
+    Router.__instance = this;
+  }
+
+  use(pathname, block) {
+    const route = new Route(pathname, block, { rootQuery: this._rootQuery });
+    this.routes.push(route);
+    return this;
+  }
+
+  start() {
+    window.onpopstate = (event) => {
+      this._onRoute(event.currentTarget.location.pathname);
+    };
+    this._onRoute(window.location.pathname);
+  }
+
+  _onRoute(pathname) {
+    const route = this.getRoute(pathname);
+
+    if (this._currentRoute) {
+      this._currentRoute.leave();
+    }
+
+    this._currentRoute = route;
+    route.render(route, pathname);
+  }
+
+  go(pathname) {
+    this.history.pushState({}, "", pathname);
+    this._onRoute(pathname);
+  }
+
+  back() {
+    const pathname =
+      this.routes[this.routes.lastIndexOf(this._currentRoute) - 1]._pathname;
+    this.history.pushState({}, "", pathname);
+    this._onRoute(pathname);
+  }
+
+  forward() {
+    const pathname =
+      this.routes[this.routes.lastIndexOf(this._currentRoute) + 1]._pathname;
+    this.history.pushState({}, "", pathname);
+    this._onRoute(pathname);
+  }
+
+  getRoute(pathname) {
+    return this.routes.find((route) => route.match(pathname));
+  }
+}
+
+export default Router;
