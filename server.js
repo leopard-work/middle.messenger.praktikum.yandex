afe09939bea0ba9dd0b3555e36726ace5b439c5c
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2023-02-28 17:49:57 +0300

    + fix server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("./dist/"));
+//app.use(express.static("./dist/"));
+app.use(express.static(path.join(__dirname, "dist")));
 
-
-const path = require('path');
+const path = require("path");
 app.use("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
